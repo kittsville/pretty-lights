a583e44e12abf3d1a7fbc1987776bf6b8ec16ec9
Kit <kittsville@googlemail.com>
2021-09-29 23:27:50 +0100

    Sets lights yellow

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pretty-lights
 IoT Lighting Toolkit
+
+Length of columns (left to right):
+21, 20, 15, 16, 14, 14
---
+++ send.py
@@ -0,0 +1,29 @@
+import socket
+import time
+
+UDP_IP = '192.168.1.56'
+UDP_PORT = 12345
+
+colours = []
+
+def genColumn(on, length):
+    colours = []
+
+    for _ in range(length):
+        hue = 40 # Yellow
+        sat = 255
+        lum = 255 if on else 0
+        colours.extend((hue, sat, lum))
+
+    return colours
+
+columns = [20, 21, 15, 16, 14, 14]
+
+for i, column in enumerate(columns):
+    colours += genColumn(True, column)
+
+MESSAGE = bytes(colours)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+print(sock.recv(4))
