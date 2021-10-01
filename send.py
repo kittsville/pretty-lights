ed48a3c974093d7183fd0e1ed3b6726e7afed6f1
kittsville <kit.maywood@ovoenergy.com>
2021-10-01 09:15:39 +0100

    Add rainbow lights

--- send.py
+++ send.py
@@ -6,21 +6,22 @@ UDP_PORT = 12345
 
 colours = []
 
-def genColumn(on, length):
+def genColumn(hue, sat, lum, length):
     colours = []
 
     for _ in range(length):
-        hue = 40 # Yellow
-        sat = 255
-        lum = 255 if on else 0
         colours.extend((hue, sat, lum))
 
     return colours
 
 columns = [20, 21, 15, 16, 14, 14]
 
-for i, column in enumerate(columns):
-    colours += genColumn(True, column)
+colours.extend(genColumn(0, 99, 89, 20))
+colours.extend(genColumn(33, 100, 100, 21))
+colours.extend(genColumn(56, 100, 100, 15))
+colours.extend(genColumn(138, 100, 50, 16))
+colours.extend(genColumn(222, 100, 100, 14))
+colours.extend(genColumn(292, 95, 53, 14))
 
 MESSAGE = b''.join([x.to_bytes(2, byteorder='big') for x in colours])
 
