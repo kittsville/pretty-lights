280d1162e35f8e5bd74bb94f3942cd30f41e4b50
Kit Maywood <kittsville@googlemail.com>
2021-10-14 11:51:57 +0100

    Refactor color into class

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import random
 import socket
 import logging
 
+from helpers.colorTools import Color
+
 UDP_IP          = '192.168.1.56'
 UDP_PORT        = 12345
 COLOR_GAP       = 40
@@ -28,20 +30,6 @@ urls = (
 )
 render = web.template.render('templates/')
 app = web.application(urls, globals())
-
-def getValue(params, name):
-    if not name in params:
-        raise web.badrequest(f'Missing param "{name}" from colour config')
-
-    try:
-        value = int(params[name])
-    except ValueError:
-        raise web.badrequest(f'{name} should be an integer, given {params[name]}')
-
-    if value < 0 or value > 255:
-        raise web.badrequest(f'{name} should be 0 <= x <= 255, given {value}')
-
-    return value
 
 def sendLedData(led_data):
     raw_led_data = bytes(led_data)
@@ -104,16 +92,14 @@ class lights:
         led_data = []
 
         for i, rawColor in enumerate(rawColors):
-            hue = getValue(rawColor, 'hue')
-            sat = getValue(rawColor, 'sat')
-            lum = getValue(rawColor, 'lum')
+            color = Color.fromDict(rawColor)
 
             num_leds_in_color_column = sum(LED_COLUMNS[i + remainder:i + remainder + num_color_columns])
 
             if i == 0:
                 num_leds_in_color_column += sum(LED_COLUMNS[i:i + remainder])
 
-            column_led_data = [hue, sat, lum] * num_leds_in_color_column
+            column_led_data = color.toList() * num_leds_in_color_column
 
             led_data.extend(column_led_data)
 
