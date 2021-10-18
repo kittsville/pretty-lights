c6a2c45f2ea2eb73f6b081e5da7bc7f84b80b001
Kit Maywood <kittsville@googlemail.com>
2021-10-18 14:43:22 +0100

    Add ColorTools from pride-flag-generator

--- static/scripts.js
+++ static/scripts.js
@@ -74,3 +74,59 @@ const colorButtons = async function() {
 }
 
 colorButtons();
+
+// Source: https://github.com/kittsville/pride-flag-generator/blob/5f3b26270a6032185dd89197b248cc60c224500f/assets/definitions.js
+class ColorTools {
+  static log(color) {
+    console.log('%c                       ', `background: ${color}`);
+  }
+
+  static gradient(firstColor, secondColor, numberOfParts) {
+    if (numberOfParts < 2) {
+      throw `Gradients require at least two parts, [${numberOfParts}] given'`
+    }
+
+    const scale = chroma.scale([firstColor, secondColor]).mode('lab');
+    const step = 1 / (numberOfParts - 1);
+
+    return Array.from({length: numberOfParts}, (_, i) => scale(step * i).hex());
+  }
+
+  static twoPartGradient(firstColor, secondColor, thirdColor, numberOfParts) {
+    const firstGradient = ColorTools.gradient(firstColor, secondColor, numberOfParts);
+    const secondGradient = ColorTools.gradient(secondColor, thirdColor, numberOfParts);
+
+    return firstGradient.concat(secondGradient.slice(1));
+  }
+
+  static randomHue() {
+    return 360 * Math.random();
+  }
+
+  static hueDistance(hue1, hue2) {
+    return Math.min(Math.abs(hue1 - hue2), (Math.min(hue1, hue2) + 360) - Math.max(hue1, hue2))
+  }
+
+  static randomPastels(number) {
+    let pastels = [];
+    let attempts = 0;
+    const limit = number * 50;
+    const separation = 70;
+
+    do {
+      attempts++;
+
+      const hue = ColorTools.randomHue();
+
+      if (!pastels.some(el => ColorTools.hueDistance(hue, el) <= separation)) {
+        pastels.push(hue);
+      }
+    } while (pastels.length < number && attempts <= limit)
+
+    if (attempts >= limit) {
+      console.log(`Gave up generating distinctive colours after ${attempts} attempts`);
+    }
+
+    return pastels.map(hue => `hsl(${hue},70%,80%)`);
+  }
+}
