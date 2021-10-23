3d5bf5e151c28e0d40b6cf00e0668cbc0578e318
Kit Maywood <kittsville@googlemail.com>
2021-10-23 22:56:36 +0100

    Extend microcontroller
    Add minimum luminocity that still illuminates LEDs
    Add sanity check

--- helpers/microcontroller.py
+++ helpers/microcontroller.py
@@ -4,10 +4,15 @@ LED_COLUMNS = [20, 21, 15, 16, 14, 14]
 NUM_LEDS    = sum(LED_COLUMNS)
 UDP_IP      = '192.168.1.56'
 UDP_PORT    = 12345
+MIN_LUM     = 36
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def sendLedData(ledData):
+    expectedBytes = NUM_LEDS * 3
+    if len(ledData) != expectedBytes:
+        raise ValueError(f'Expected {expectedBytes} bytes, got {len(ledData)}')
+
     rawLedData = bytes(ledData)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
